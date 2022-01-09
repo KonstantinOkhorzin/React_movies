fa3394a4c1ec54c1a7c7f56541338b2bcb93f790
Konstantin Okhorzin <konstantinokhorzin@gmail.com>
2022-01-09 16:39:19 +0200

    fail search

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,12 +3,12 @@ import React from 'react';
 import Movie from './Movie';
 
 function Movies(props) {
-    const { movies } = props;
+    const { movies = [] } = props;
     return (
         <div className='movies'>
-            { movies.map(movie => {
+            { movies.length ? movies.map(movie => {
                 return <Movie key={movie.imdbID} {...movie}/>
-            })}
+            }) : <h4>Nothing found</h4>}
         </div>
     );
 }
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -7,26 +7,30 @@ import Search from '../components/Search';
 function Main() {
 
     const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch('http://www.omdbapi.com/?apikey=78584b3c&s=matrix')
         .then(response => response.json())
         .then(data => setMovies(data.Search))
+        setLoading(false)
     }, []);
 
     const searchMovies = (str, type) => {
+        setLoading(true)
         fetch(`http://www.omdbapi.com/?apikey=78584b3c&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
         .then(response => response.json())
         .then(data => setMovies(data.Search))
+        setLoading(false)
     }
 
     return (
         <main className='content container'>
             <Search searchMovies={searchMovies}/>
            {
-               movies.length ? 
-               (<Movies movies={movies}/>) :
-               <Preloader/>
+               loading ? 
+               <Preloader/> :
+               <Movies movies={movies}/>
            }
         </main>
     );
