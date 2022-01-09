7562421cec52c77201d93a3aa37dc59945b150c9
Konstantin Okhorzin <konstantinokhorzin@gmail.com>
2022-01-09 08:41:27 +0200

    Movies and Movie

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
+
+import Movies from '../components/Movies';
 
 function Main() {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch('http://www.omdbapi.com/?apikey=78584b3c&s=matrix')
+        .then(response => response.json())
+        .then(data => setMovies(data.Search))
+    }, [movies])
+
     return (
         <main className='content container'>
-            Hello
+           {
+               movies.length ? 
+               (<Movies movies={movies}/>) :
+               <h3>Loading...</h3>
+           }
         </main>
     );
 }
